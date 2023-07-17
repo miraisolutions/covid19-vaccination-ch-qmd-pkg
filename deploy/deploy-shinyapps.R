6663f37c10973ae93da44234ec4bbbcc2b88bab7
GuidoMaggio <guido.maggio@mirai-solutions.com>
2023-07-17 19:57:32 +0200

    render local

--- deploy/deploy-shinyapps.R
+++ deploy/deploy-shinyapps.R
@@ -6,14 +6,14 @@ rsconnect::setAccountInfo(
   Sys.getenv("SHINYAPPS_SECRET")
 )
 
-file.copy("inst/report/inst_index.qmd", "inst_index.qmd", overwrite = TRUE)
+file.copy("inst/report/index.qmd", "index.qmd", overwrite = TRUE)
 
 
 quarto::quarto_publish_app(
-  input = "inst_index.qmd",
+  input = "index.qmd",
   server = "shinyapps.io",
   name = "covid-19-vaccination-ch-qmd-pkg",
   title = "Covid19 Vaccination CH",
   account = "miraisolutions",
-  render = "server"
+  render = "local"
 )
