a7063430179c727b4054011bc124684aea240930
GuidoMaggio <guido.maggio@mirai-solutions.com>
2023-08-30 11:52:26 +0200

    Productive version

--- deploy/deploy-shinyapps.R
+++ deploy/deploy-shinyapps.R
@@ -10,7 +10,7 @@ file.copy("inst/report/index.qmd", "index.qmd", overwrite = TRUE)
 quarto::quarto_publish_app(
   input = "index.qmd",
   server = "shinyapps.io",
-  name = "covid19-vaccination-ch-qmd-test",
+  name = "covid19-vaccination-ch-qmd",
   title = "Covid19 Vaccination CH",
   account = "miraisolutions",
   render = "local",
